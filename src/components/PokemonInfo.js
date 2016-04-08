d3d515a9dc18f58b001c564f9fd8876ad5d9ce65
vnc <cabreravnc@yahoo.com>
2016-04-08 22:02:23 +0800

    bebas neue font

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const InfoHeader = () => (
-  <div className="infoheader row col-md-12">
-    <div className="col-md-9">
+  <div className="infoheaderWrapper row col-md-12">
+    <div className="infoheader col-md-10">
       <span class="japanese">JAPANESE FUSHIGIDANE</span>
       <span class="gender">GENDER RATE 1</span>
       <span class="catch">CATCH RATE 45</span>
     </div>
-    <div className="ledWrapper col-md-3 pull-right">
+    <div className="ledWrapper col-md-2 pull-right">
       <div className="led"></div>
     </div>
   </div>
